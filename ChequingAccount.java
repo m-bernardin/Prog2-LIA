09564daca6bfb61aeacf10d1f8abff1100c76549
m-bernardin <mjrb4mjrb@gmail.com>
2026-03-30 17:13:07 -0400

    accounts structure, unimplemented

--- ChequingAccount.java
+++ ChequingAccount.java
@@ -1,11 +1,11 @@
-public class SavingsAccount extends EarningsAccount{
-    public SavingsAccount(double balance) {
-        super(balance, 0.02);
+public class ChequingAccount extends Account{
+    public ChequingAccount(double balance) {
+        super(balance);
+        //TODO Auto-generated constructor stub
     }
     @Override
     public boolean withdraw(double amnt) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
     }
-
 }
---
+++ InvestmentAccount.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+public class InvestmentAccount extends EarningsAccount{
+    private LocalDate dateOpened;
+    public InvestmentAccount(double balance,LocalDate dateOpened) {
+        super(balance, 0.05);
+        this.dateOpened = dateOpened;
+    }
+    public LocalDate getDateOpened() {
+        return dateOpened;
+    }
+    public void setDateOpened(LocalDate dateOpened) {
+        this.dateOpened = dateOpened;
+    }
+    @Override
+    public boolean withdraw(double amnt) {
+        // TODO Auto-generated method stub
+        // TODO throws clause
+        throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+    }
+}
