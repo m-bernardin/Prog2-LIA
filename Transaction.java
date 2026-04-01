c446121d78600e78f90eeae7f8f02e89b60e15f5
m-bernardin <mjrb4mjrb@gmail.com>
2026-04-01 15:41:30 -0400

    transaction complete

--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,36 @@
-public class Transaction {
+public class Transaction{
     private String transactionID;
+    private double amnt;
+    private String receivingAccount;
+    private String givingAccount;
+    public Transaction(String transactionID, double amnt, String receivingAccount, String givingAccount) {
+        this.transactionID = transactionID;
+        this.amnt = amnt;
+        this.receivingAccount = receivingAccount;
+        this.givingAccount = givingAccount;
+    }
+    public String getTransactionID() {
+        return transactionID;
+    }
+    public void setTransactionID(String transactionID) {
+        this.transactionID = transactionID;
+    }
+    public double getAmnt() {
+        return amnt;
+    }
+    public void setAmnt(double amnt) {
+        this.amnt = amnt;
+    }
+    public String getReceivingAccount() {
+        return receivingAccount;
+    }
+    public void setReceivingAccount(String receivingAccount) {
+        this.receivingAccount = receivingAccount;
+    }
+    public String getGivingAccount() {
+        return givingAccount;
+    }
+    public void setGivingAccount(String givingAccount) {
+        this.givingAccount = givingAccount;
+    }
 }
