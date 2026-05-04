c05c325983b9c6ab38b1cd7b8ac1fd698865df8a
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-04 14:42:30 -0400

    blank account javadocs

--- prog2-lia/src/main/java/com/example/BlankAccount.java
+++ prog2-lia/src/main/java/com/example/BlankAccount.java
@@ -1,18 +1,30 @@
 package com.example;
-
+/**
+ * A child of Account which describes an account with no details. Used by the DashboardController when a Client has no open Accounts.
+ * @see DashboardController
+ */
 public class BlankAccount extends Account{
-
+    /**
+     * Basic constructor for this class. Sets the accountID to a generic 'X'.
+     */
     public BlankAccount() throws InvalidTypeException {
         super();
         accountID="X";
     }
+    /**
+     * Returns a representation of this class as a String.
+     * @return a message to be displayed to the user, saying no accounts are open.
+     */
     @Override
     public String toString() {
         return "No accounts to show...";
     }
+    /**
+     * Implements the parent's method to do nothing.
+     * @throws InsufficientFundsException always.
+     */
     @Override
     public void withdraw(double amnt) throws InvestmentLockException, InsufficientFundsException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+        throw new InsufficientFundsException();
     }
 }
