e70761d445d56af8073f9147ddf15361c66f22a7
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-04 14:59:59 -0400

    chequeing account javadocs

--- prog2-lia/src/main/java/com/example/ChequeingAccount.java
+++ prog2-lia/src/main/java/com/example/ChequeingAccount.java
@@ -1,9 +1,19 @@
 package com.example;
-
+/**
+ * Represents a chequeing account. Essentially a concrete implementtion of the parent.
+ */
 public class ChequeingAccount extends Account{
+    /**
+     * Basic constrcutor for this class. IdCreator type (2,1).
+     */
     public ChequeingAccount() throws InvalidTypeException {
         accountID=IdCreator.createID(2,1);
     }
+    /**
+     * Withdraws a specified amount from this account.
+     * @param amnt - the amount to be withdrawn.
+     * @throws InsufficientFundsException if this account does not have sufficient funds for this withdrawal.
+     */
     @Override
     public void withdraw(double amnt) throws InsufficientFundsException{
         if(balance>=amnt){
@@ -12,6 +22,10 @@ public class ChequeingAccount extends Account{
         }
         throw new InsufficientFundsException();
     }
+    /**
+     * Returns a reperesentation of this acccount as a String.
+     * @return the representation.
+     */
     @Override
     public String toString() {
         return "Chequeing account no. "+accountID+"\tbalance: "+balance+"$";
