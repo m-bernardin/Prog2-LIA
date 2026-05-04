350587c77421c93a39676f21e5ca9eb88ad6022d
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-04 17:41:51 -0400

    client javadocs

--- prog2-lia/src/main/java/com/example/Client.java
+++ prog2-lia/src/main/java/com/example/Client.java
@@ -1,78 +1,132 @@
 package com.example;
+
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.ArrayList;
 import java.util.HashSet;
+/**
+ * Represents a client at its most basic form.
+ */
 public abstract class Client implements Maintainable{
-    // fields
-    private String clientID;
-    protected HashSet<String> accounts=new HashSet<>();
-    private String username;
-    private String password;
-    protected HashSet<String> transactions=new HashSet<>();
-    private LocalDate dateLastOpened;
-    private LocalDate dateOpened;
-    protected int monthlyFee=10;
+    /**
+     * Percentage of extra interest applied to monthly interest on earnings accounts.
+     */
     protected static final double EXTRA_INTEREST=0;
-    // constructors
+    /**
+     * This client's unqiue ID.
+     * IdCreator type 1.
+     */
+    private String clientID;
+    /**
+     * Set of the unqiue IDs of all accounts owned by this client.
+     */
+    protected HashSet<String> accounts=new HashSet<>();
+    /**
+     * This client's username, used for login.
+     */
+    private String username;
+    /**
+     * This client's password, used for login.
+     */
+    private String password;
+    /**
+     * Set of unique IDs of all transactions associated with this account.
+     */
+    protected HashSet<String> transactions=new HashSet<>();
+    /**
+     * The date that this client last logged in.
+     */
+    private LocalDate dateLastOpened;
+    /**
+     * The date that this client first logged in.
+     */
+    private LocalDate dateOpened;
+    /**
+     * The amount this client must pay each month per owned account.
+     */
+    protected int monthlyFee=10;
+    /**
+     * The basic constructor for this class. Takes a username and password, then auto-generates the rest.
+     * @param username - the client's username.
+     * @param password - the client's password.
+     */
     public Client(String username,String password){
         this.username=username;
         this.password=password;
         accounts=new HashSet<>();
         transactions=new HashSet<>();
-        clientID="XXXXXX";
+        clientID="";
         dateOpened=LocalDate.now();
     }
-    // getters and setters
-    public String getClientID() {
-        return clientID;
-    }
+    /**
+     * Sets the client's ID to the specified ID.
+     * @param clientID - the ID to be changed to.
+     */
     public void setClientID(String clientID) {
         this.clientID = clientID;
     }
-    public HashSet<String> getAccounts() {
-        return accounts;
-    }
-    public void setAccounts(HashSet<String> accounts) {
-        this.accounts = accounts;
-    }
+    /**
+     * Gets the client's username.
+     * @return - the client's username.
+     */
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    /**
+     * Gets the client's password.
+     * @return - the client's password.
+     */
     public String getPassword() {
         return password;
     }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public HashSet<String> getTransactions() {
-        return transactions;
-    }
-    public void setTransactions(HashSet<String> transactions) {
-        this.transactions = transactions;
-    }
-    public LocalDate getDateLastOpened() {
-        return dateLastOpened;
-    }
+    /**
+     * Sets the date this client last logged in to the specified date.
+     * @param dateLastOpened - the date to be changed to.
+     */
     public void setDateLastOpened(LocalDate dateLastOpened) {
         this.dateLastOpened = dateLastOpened;
     }
-    public LocalDate getDateOpened() {
-        return dateOpened;
+    /**
+     * Gets the client's ID.
+     * @return the client's ID.
+     */
+    public String getClientID() {
+        return clientID;
     }
-    public void setDateOpened(LocalDate dateOpened) {
-        this.dateOpened = dateOpened;
+    /**
+     * Gets the client's owned accounts' IDs.
+     * @return a set of the accounts' IDs.
+     */
+    public HashSet<String> getAccounts() {
+        return accounts;
     }
-    public int getMonthlyFee() {
-        return monthlyFee;
+    /**
+     * Implementation of the Maintainable interface. Defines what must be done each time this client logs in.
+     * @return true if the maintenance is successful; false otherwise.
+     * @throws InvalidTypeException if something went wrong on a deeper level.
+     */
+    @Override
+    public boolean maintain() throws InvalidTypeException {
+        Period periodSinceLastOpened=Period.between(LocalDate.now(), dateLastOpened);
+        int monthsSinceLastOpened=periodSinceLastOpened.getMonths()+periodSinceLastOpened.getYears()*12;
+        boolean sufficientFunds=true;
+        for(int i=0;i<monthsSinceLastOpened;++i){
+            sufficientFunds=applyMonthlyFee();
+            for (String accountID : App.driver.getOwnedEarningsAccounts(clientID)) {
+                try {
+                    App.driver.getEarningsAccount(accountID).applyInterest();
+                } catch (Exception e) {
+                    System.out.println("**non eaarnings account");
+                }
+            }
+        }
+        setDateLastOpened(LocalDate.now());
+        return sufficientFunds;
     }
-    public void setMonthlyFee(int monthlyFee) {
-        this.monthlyFee = monthlyFee;
-    }
-    // abstract methods
+    /**
+     * Charges monthly fees to this client's primary chequeing account.
+     * @return true if this operation was successful; false otherwise.
+     * @throws InvalidTypeException if something went wrong on a deeper level.
+     */
     protected boolean applyMonthlyFee() throws InvalidTypeException{
         boolean success=true;
         try {
@@ -87,30 +141,18 @@ public abstract class Client implements Maintainable{
             App.displayError("Insufficient funds to charge monthly fees");
         } return success;
     }
-    // concrete methods
-    protected boolean addAccount(String account){
-        accounts.add(account);
-        return true;
+    /**
+     * Adds a account as an owned account, based off a specified account ID.
+     * @param accountID - the account ID to be added.
+     */
+    protected void addAccount(String accountID){
+        accounts.add(accountID);
     }
+    /**
+     * Gets the total amount this client must pay each omth for their open accounts.
+     * @return the amount they must pay.
+     */
     protected int calculateTotalMonthlyFee(){
         return monthlyFee*accounts.size();
-    }
-    @Override
-    public boolean maintain() throws InvalidTypeException {
-        Period periodSinceLastOpened=Period.between(LocalDate.now(), dateLastOpened);
-        int monthsSinceLastOpened=periodSinceLastOpened.getMonths()+periodSinceLastOpened.getYears()*12;
-        boolean sufficientFunds=true;
-        for(int i=0;i<monthsSinceLastOpened;++i){
-            sufficientFunds=applyMonthlyFee();
-            for (String accountID : App.driver.getOwnedEarningsAccounts(clientID)) {
-            try {
-                App.driver.getEarningsAccount(accountID).applyInterest();
-            } catch (Exception e) {
-                System.out.println("**non eaarnings account");
-            }
-            }
-        }
-        setDateLastOpened(LocalDate.now());
-        return sufficientFunds;
     }
 }
