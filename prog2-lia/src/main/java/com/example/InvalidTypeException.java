73e6593a912d740faaf97dcd15a0c6bfa9ccff32
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 16:47:45 -0400

    finished invalid type exception javadocs

--- prog2-lia/src/main/java/com/example/InvalidTypeException.java
+++ prog2-lia/src/main/java/com/example/InvalidTypeException.java
@@ -5,7 +5,10 @@ package com.example;
  * @author Mathieu Bernardin
  */
 public class InvalidTypeException extends Exception{
-
+    /**
+     * Constructor for this exception. Forces a message to be included with the exception throw.
+     * @param message
+     */
     public InvalidTypeException(String message) {
         super(message);
     }
