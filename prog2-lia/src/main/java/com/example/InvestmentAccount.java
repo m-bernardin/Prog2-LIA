c0491c679941bc11400e3cf16d47fffa6c12913e
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 16:55:37 -0400

    investment account javadocs

--- prog2-lia/src/main/java/com/example/InvestmentAccount.java
+++ prog2-lia/src/main/java/com/example/InvestmentAccount.java
@@ -1,8 +1,19 @@
 package com.example;
 
 import java.time.LocalDate;
+/**
+ * A type of earnings account which cannot be withdrawn from until a year after being opened.
+ * @author Mathieu Bernardin
+ */
 public class InvestmentAccount extends EarningsAccount{
+    /**
+     * The date this account was opened.
+     */
     private LocalDate dateOpened;
+    /**
+     * Constructor for this class. Automatically generates an account ID, the interest for this account, and the date this account was opened.
+     * @throws InvalidTypeException if something goes wrong.
+     */
     public InvestmentAccount() throws InvalidTypeException {
         super();
         boolean validID=false;
@@ -13,12 +24,19 @@ public class InvestmentAccount extends EarningsAccount{
         interestRate=0.05;
         dateOpened=LocalDate.now();
     }
+    /**
+     * Gets the date this account was opened.
+     * @return this accounts opening date.
+     */
     public LocalDate getDateOpened() {
         return dateOpened;
     }
-    public void setDateOpened(LocalDate dateOpened) {
-        this.dateOpened = dateOpened;
-    }
+    /**
+     * Withdraws a specified amount from this account, but only if it has been open for more than a year.
+     * @param amnt - the amount to be withdrawn.
+     * @throws InsufficientFundsException if there are not enough funds for the withdrawal in this account.
+     * @throws InvestmentLockException if this account has been open for less than a year.
+     */
     @Override
     public void withdraw(double amnt) throws InvestmentLockException, InsufficientFundsException {
         if(dateOpened.plusYears(1).isBefore(LocalDate.now())){
@@ -30,6 +48,10 @@ public class InvestmentAccount extends EarningsAccount{
         }
         throw new InvestmentLockException(LocalDate.now().until(dateOpened.plusYears(1)).toString());
     }
+    /**
+     * Gets a String representation of this account.
+     * @return the representation of this account.
+     */
     @Override
     public String toString() {
         return "Investment account no. "+accountID+"\tbalance: "+balance+"$";
