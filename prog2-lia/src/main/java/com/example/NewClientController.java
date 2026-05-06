6c3d324893f51368ed56ab8ff6ee3b05452224f3
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 17:14:23 -0400

    new client controller javadocs

--- prog2-lia/src/main/java/com/example/NewClientController.java
+++ prog2-lia/src/main/java/com/example/NewClientController.java
@@ -1,21 +1,48 @@
 package com.example;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.CheckBox;
-
+/**
+ * FXML controller for the new client pages.
+ * @author Mathieu Bernardin
+ */
 public class NewClientController {
+    /**
+     * Link to the field where the user enters their username.
+     */
     @FXML TextField usernameField;
+    /**
+     * Link to the field where the user enters their password.
+     */
     @FXML TextField passwordField;
+    /**
+     * Link to the field where the user enters their name or company's name.
+     */
     @FXML TextField nameField;
+    /**
+     * Link to the field where the user enters their contact.
+     */
     @FXML TextField emailField;
+    /**
+     * Link to the selector for client type.
+     */
     @FXML ComboBox<String> typeSelector;
+    /**
+     * Link to the checkbox where the user may opt into the rewards program, where applicable.
+     */
     @FXML CheckBox rewardsProgramCheck;
+    /**
+     * Allows for creation of individual clients based off entered info.
+     * @param event - the FXML event.
+     * @throws IOException if something goes wrong.
+     * @throws InvalidTypeException if something goes wrong.
+     */
     @FXML
     private void createIndividualClient(ActionEvent event) throws IOException, InvalidTypeException{
         String username=usernameField.getText();
@@ -30,6 +57,11 @@ public class NewClientController {
         App.driver.createIndividualClient(username, passsword, name, contact);
         App.setRoot("login");
     }
+    /**
+     * Advances to the next page, based off the user's selection.
+     * @param event - the FXML event
+     * @throws IOException if something goes wrong.
+     */
     @FXML
     private void confirmSelection(ActionEvent event) throws IOException{
         String choice=typeSelector.getValue();
@@ -45,7 +77,12 @@ public class NewClientController {
             App.displayError("Invalid selection...");
         }
     }
-
+    /**
+     * Allows for creation of student clients based off entered info.
+     * @param event - the FXML event.
+     * @throws IOException if something goes wrong.
+     * @throws InvalidTypeException if something goes wrong.
+     */
     @FXML
     private void createStudentClient(ActionEvent event) throws IOException, InvalidTypeException{
         String username=usernameField.getText();
@@ -60,6 +97,12 @@ public class NewClientController {
         App.driver.createStudentClient(username, passsword, name, contact);
         App.setRoot("login");
     }
+    /**
+     * Allows for creation of corporate clients based off entered info.
+     * @param event - the FXML event.
+     * @throws IOException if something goes wrong.
+     * @throws InvalidTypeException if something goes wrong.
+     */
     @FXML
     private void createCorporateClient(ActionEvent event) throws IOException, InvalidTypeException{
         String username=usernameField.getText();
@@ -76,6 +119,11 @@ public class NewClientController {
         App.driver.createCorporateClient(username, passsword, name, clientManagerContacts, rewardsProgramMember);
         App.setRoot("login");
     }
+    /**
+     * Parses emails from a specified String into an ArrayList.
+     * @param contact - the String to be parsed.
+     * @return the parsed emails.
+     */
     private ArrayList<String> parseEmails(String contact) {
         Scanner parser=new Scanner(contact).useDelimiter(",");
         ArrayList<String> emails=new ArrayList<>();
@@ -87,6 +135,12 @@ public class NewClientController {
         parser.close();
         return emails;
     }
+    /**
+     * Allows for creation of vip clients based off entered info.
+     * @param event - the FXML event.
+     * @throws IOException if something goes wrong.
+     * @throws InvalidTypeException if something goes wrong.
+     */
     @FXML
     private void createVipClient(ActionEvent event) throws IOException, InvalidTypeException{
         String username=usernameField.getText();
@@ -102,6 +156,10 @@ public class NewClientController {
         App.driver.createVipClient(username, passsword, rewardsProgramMember, name, contact);
         App.setRoot("login");
     }
+    /**
+     * Cancels client registration and returns to the login page.
+     * @throws IOExceptionss if something goes wrong.
+     */
     @FXML
     private void cancel() throws IOException{
         App.setRoot("login");
