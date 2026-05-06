adcb640c21ab97ab38614b00296abdab1449a793
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 17:44:31 -0400

    student client javadocs

--- prog2-lia/src/main/java/com/example/StudentClient.java
+++ prog2-lia/src/main/java/com/example/StudentClient.java
@@ -1,9 +1,27 @@
 package com.example;
 
 import java.time.LocalDate;
+/**
+ * A typoe of standard client belonging to a full time student. Account ownership fees are waived for them.
+ * @author Mathieu Bernardin
+ */
 public class StudentClient extends StandardClient{
+    /**
+     * The date this client's full time student status expires.
+     */
     private LocalDate statusExpiryDate;
+    /**
+     * If this client's full time student status if currently valid.
+     */
     private boolean statusValid;
+    /**
+     * Constructor for this class. Automatically generates a client ID (type (1,2)).
+     * @param username - this client's username.
+     * @param password - this client's password.
+     * @param name - this client's name.
+     * @param contact - this client's contact.
+     * @throws InvalidTypeException if something goes wrong.
+     */
     public StudentClient(String username, String password, String name, String contact) throws InvalidTypeException {
         super(username, password, name, contact);
         boolean validID=false;
@@ -15,22 +33,40 @@ public class StudentClient extends StandardClient{
         setClientID(ID);
         statusValid=false;
     }
+    /**
+     * Gets the date this client's full time student status expires.
+     * @return the date this client's full time student status expires.
+     */
     public LocalDate getStatusExpiryDate() {
         return statusExpiryDate;
     }
+    /**
+     * Sets this client's full time student status to the specified date.
+     * @param statusExpiryDate the new date of this client's full time student status.
+     */
     public void setStatusExpiryDate(LocalDate statusExpiryDate) {
         this.statusExpiryDate = statusExpiryDate;
     }
+    /**
+     * If this client's full time student status is currently valid.
+     * @return this client's full time student status.
+     */
     public boolean isStatusValid() {
         return statusValid;
     }
+    /**
+     * Sets this client's full time student status to the specified quality.
+     * @param statusValid the new quality of client's full time student status.
+     */
     public void setStatusValid(boolean statusValid) {
         this.statusValid = statusValid;
     }
+    /**
+     * Verifies this client's full time student status is still valid, if its invalid, updates it accordingly.
+     */
     @Override
     public boolean maintain() throws InvalidTypeException {
-        boolean sufficientFunds=super.maintain();
         if(LocalDate.now().isAfter(statusExpiryDate))statusValid=false;
-        return sufficientFunds;
+        return true;
     }
 }
