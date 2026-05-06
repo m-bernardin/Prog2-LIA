8916f9be7514c9b8db96b04f98ca13c8b6834d35
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 17:18:30 -0400

    premium client javadocs

--- prog2-lia/src/main/java/com/example/PremiumClient.java
+++ prog2-lia/src/main/java/com/example/PremiumClient.java
@@ -1,16 +1,28 @@
 package com.example;
-
+/**
+ * Type of client which may opt into the rewards program.
+ * @author Mathieu Bernardin
+ */
 public abstract class PremiumClient extends Client{
+    /**
+     * Constructor for this class.
+     * @param username - this client's username.
+     * @param password - this client's password.
+     * @param rewardsProgramMember - if this client has opted into the rewards program.
+     */
     public PremiumClient(String username, String password,boolean rewardsProgramMember) {
         super(username, password);
         this.rewardsProgramMember=rewardsProgramMember;
     }
+    /**
+     * If this user has opted into the rewards program.
+     */
     private boolean rewardsProgramMember;
-    
+    /**
+     * Gets this client's reward program status.
+     * @return if this client has opted into the rewards program.
+     */
     public boolean isRewardsProgramMember() {
         return rewardsProgramMember;
-    }
-    public void setRewardsProgramMember(boolean rewardsProgramMember) {
-        this.rewardsProgramMember = rewardsProgramMember;
     }
 }
