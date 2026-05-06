5614e2ae79f48ceb40a0e24c8f294410255f4dc6
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 16:42:02 -0400

    individual client javadocs

--- prog2-lia/src/main/java/com/example/IndividualClient.java
+++ prog2-lia/src/main/java/com/example/IndividualClient.java
@@ -1,6 +1,16 @@
 package com.example;
-
+/**
+ * Represents the most basic kind of client.
+ */
 public class IndividualClient extends StandardClient{
+    /**
+     * Constructor for this class. Automatically creates client ID.
+     * @param username - the client's username.
+     * @param password - the client's password.
+     * @param name - the client's name.
+     * @param contact - the client's contact.
+     * @throws InvalidTypeException if something goes wrong.
+     */
     public IndividualClient(String username, String password, String name, String contact) throws InvalidTypeException {
         super(username, password, name, contact);
         boolean validID=false;
