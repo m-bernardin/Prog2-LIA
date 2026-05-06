ccaf530277e8eb46c25171e181a9f7f56e63e40f
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 17:01:21 -0400

    login controller javadocs

--- prog2-lia/src/main/java/com/example/LoginController.java
+++ prog2-lia/src/main/java/com/example/LoginController.java
@@ -5,30 +5,39 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+/**
+ * The FXML controller for the login page.
+ * @author Mathieu Bernardin
+ */
 public class LoginController{
+    /**
+     * Link to the field where the user enters their username.
+     */
     @FXML private TextField usernameField;
+    /**
+     * Link to the field where the user enters their password.
+     */
     @FXML private TextField passwordField;
-    @FXML private Button loginButton;
+    /**
+     * Allows the loginButton to function. Attempts to log the user in.
+     * @param event - the FXML event.
+     * @throws IOException if something goes wrong.
+     */
     @FXML
     private void login(ActionEvent event) throws IOException{
-        System.out.println("**login pressed!");
-        System.out.println("**username: "+usernameField.getText());
-        System.out.println("**password: "+passwordField.getText());
         if(App.driver.login(usernameField.getText(),passwordField.getText())){
             App.setRoot("dashboard");
-        }
-        else 
-        {
+        } else{
             usernameField.clear();
             passwordField.clear();
-            Alert invalidAlert=new Alert(AlertType.ERROR);
-            invalidAlert.setContentText("Invalid username or password... Please try again");
-            invalidAlert.show();
+            App.displayError("Invalid username or password... Please try again");
         }
     }
+    /**
+     * Allows the newClientButton to function. Navigates the user to the newClient page.
+     */
     @FXML
     private void newClient() throws IOException{
-        System.out.println("**new client pressed");
         App.setRoot("newClient");
     }
 }
