278984aa78fcc847f85d844dbdd5ef6a6fed9d44
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-06 17:04:44 -0400

    missing file exception javadocs

--- prog2-lia/src/main/java/com/example/MissingChequingException.java
+++ prog2-lia/src/main/java/com/example/MissingChequingException.java
@@ -1,9 +1,6 @@
 package com.example;
-
 /**
  * Thrown when a user attempts an operation which requires a chequing account without currently having one.
  * @author Mathieu Bernardin
  */
-public class MissingChequingException extends Exception{
-    
-}
+public class MissingChequingException extends Exception{}
--- prog2-lia/src/main/java/com/example/MissingFileException.java
+++ prog2-lia/src/main/java/com/example/MissingFileException.java
@@ -1,5 +1,13 @@
 package com.example;
+/**
+ * Thrown when the driver attempts to access a file which does not exist or is otherwise inaccessible.
+ * @author Mathieu Bernardin
+ */
 public class MissingFileException extends Exception{
+    /**
+     * Constructor for this class. Forces a message to be specified when throwing this exception.
+     * @param message - the specified message.
+     */
     public MissingFileException(String message) {
         super(message);
     }
