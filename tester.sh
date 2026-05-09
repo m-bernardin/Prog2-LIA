71c2ba544530bc6ee9512c72965e7d2a12683e7c
m-bernardin <mjrb4mjrb@gmail.com>
2026-05-09 15:34:11 -0400

    tester script

---
+++ tester.sh
@@ -0,0 +1,28 @@
+echo Running tests...
+cd ~/GitHub/Prog2-LIA/prog2-lia
+mvn test -X > ../log.txt
+echo '
+Account tests:
+' >> ../log.txt
+cat src/target/surefire-reports/com.example.AccountTest.txt >> ../log.txt 2>/dev/null
+echo '
+App tests:
+' >> ../log.txt
+cat src/target/surefire-reports/com.example.AppTest.txt >> ../log.txt 2>/dev/null
+echo '
+Client tests:
+' >> ../log.txt
+cat src/target/surefire-reports/com.example.ClientTest.txt >> ../log.txt 2>/dev/null
+echo '
+Driver tests:
+' >> ../log.txt
+cat src/target/surefire-reports/com.example.DriverTest.txt >> ../log.txt 2>/dev/null
+echo '
+IdCreator tests:
+' >> ../log.txt
+cat src/target/surefire-reports/com.example.IdCreatorTest.txt >> ../log.txt 2>/dev/null
+echo '
+Transaction tests:
+' >> ../log.txt
+cat src/target/surefire-reports/com.example.TransactionTest.txt >> ../log.txt 2>/dev/null
+echo Tests complete! Please see log.txt for details
